13d620d9950fd6f37757c95306a8a716dc54904a
go-to-k <24818752+go-to-k@users.noreply.github.com>
2022-12-14 23:17:47 +0900

    feat: custom resources

---
+++ lib/resource/ts-cdk-ts-managed-apprunner-stack.custom.ts
@@ -0,0 +1,72 @@
+import {
+  AppRunnerClient,
+  CreateAutoScalingConfigurationCommand,
+  DeleteAutoScalingConfigurationCommand,
+  ListAutoScalingConfigurationsCommand,
+} from "@aws-sdk/client-apprunner";
+import { CdkCustomResourceEvent, CdkCustomResourceResponse } from "aws-lambda";
+
+interface InputProps {
+  autoScalingConfigurationName: string;
+  maxConcurrency: number;
+  maxSize: number;
+  minSize: number;
+}
+
+const appRunnerClient = new AppRunnerClient({
+  region: process.env.REGION,
+});
+
+export const handler = async function (
+  event: CdkCustomResourceEvent,
+): Promise<CdkCustomResourceResponse> {
+  const data: { [key: string]: string } = {};
+  const requestType = event.RequestType;
+  const input: InputProps = {
+    autoScalingConfigurationName: event.ResourceProperties["AutoScalingConfigurationName"],
+    maxConcurrency: event.ResourceProperties["MaxConcurrency"],
+    maxSize: event.ResourceProperties["maxSize"],
+    minSize: event.ResourceProperties["minSize"],
+  };
+
+  if (requestType === "Create") {
+    const createAutoScalingConfigurationCommand = new CreateAutoScalingConfigurationCommand({
+      AutoScalingConfigurationName: input.autoScalingConfigurationName,
+      MaxConcurrency: input.maxConcurrency,
+      MaxSize: input.maxSize,
+      MinSize: input.minSize,
+    });
+
+    const createAutoScalingConfigurationResponse = await appRunnerClient.send(
+      createAutoScalingConfigurationCommand,
+    );
+
+    data["AutoScalingConfigurationArn"] =
+      createAutoScalingConfigurationResponse.AutoScalingConfiguration
+        ?.AutoScalingConfigurationArn ?? "";
+  } else if (requestType === "Delete") {
+    const listAutoScalingConfigurationCommand = new ListAutoScalingConfigurationsCommand({
+      AutoScalingConfigurationName: input.autoScalingConfigurationName,
+    });
+
+    const listAutoScalingConfigurationsResponse = await appRunnerClient.send(
+      listAutoScalingConfigurationCommand,
+    );
+
+    if (listAutoScalingConfigurationsResponse.AutoScalingConfigurationSummaryList?.length) {
+      const autoScalingConfigurationArn =
+        listAutoScalingConfigurationsResponse.AutoScalingConfigurationSummaryList[0]
+          .AutoScalingConfigurationArn;
+      const deleteAutoScalingConfigurationCommand = new DeleteAutoScalingConfigurationCommand({
+        AutoScalingConfigurationArn: autoScalingConfigurationArn ?? "",
+      });
+
+      await appRunnerClient.send(deleteAutoScalingConfigurationCommand);
+    }
+  }
+
+  return {
+    PhysicalResourceId: "AutoScalingConfiguration",
+    Data: data,
+  };
+};
