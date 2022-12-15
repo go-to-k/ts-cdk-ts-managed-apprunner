2c2e5d76d2d8833239433bafeff78d9ccc3b1945
go-to-k <24818752+go-to-k@users.noreply.github.com>
2022-12-15 20:38:09 +0900

    fix: app for index.ts

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
+
 const app: express.Express = express();
 const outputValue = process.env?.ENV1 ?? "Hello World";
+
+app.get("/", (req, res) => {
+  res.send(outputValue);
+});
+
 app.listen(8080, () => {
   console.log(outputValue);
 });
